ef742f2c4fe81e4cbefa2aac26c3321bf5d5be1c
René van Aerle <info@rvgate.nl>
2016-09-07 01:33:11 +0200

    Fetch match details from Biljartpoint

---
+++ biljartpoint.php
@@ -0,0 +1,95 @@
+<?php
+include('lib/simple_html_dom.php');
+
+$page = $_GET['page'];
+$bondsNr = $_GET['bondsnr'];
+$klasse = $_GET['klasse'];
+$district = $_GET['d'];
+
+$seasons = [
+    '2013-2014',
+    '2014-2015',
+    '2015-2016',
+    '2016-2017',
+    '2017-2018',
+    '2018-2019',
+    '2019-2020',
+    '2020-2021',
+];
+
+function fetchPlayerAndMatches($page, $bondsNr, $klasse, $seasons, $district) {
+    // fetch html
+    $url = 'http://biljartpoint.nl/';
+    $query = [
+        'page' => $page,
+        'bondsnr' => $bondsNr,
+        'klasse' => $klasse,
+        'seizoen' => '',
+        'd' => $district
+    ];
+
+    $playerName = null;
+    $matches = [];
+    foreach($seasons as $index => $season) {
+        $query['seizoen'] = $season;
+        $html = file_get_html($url . '?' . http_build_query($query));
+        if($index == 0) {
+            $playerName = getPlayerName($bondsNr, $season, $html->find('h1')[0]->plaintext);
+        }
+        $tables = $html->find('table');
+        if(strpos($tables[0], 'Er zijn nog geen resultaten bekend voor deze speler dit seizoen.') === false) {
+            foreach($tables[3]->find('tr') as $match) {
+                array_push($matches, $match);
+            }
+        }
+    }
+    return [$playerName, $matches];
+}
+
+function getPlainText($td) {
+    $string = trim($td->plaintext);
+    return $string;
+}
+function formatOpponent($string) {
+    $string = str_replace('  ', ' ', $string);
+    $string = preg_replace('~[\r\n\t]+~', ' ', $string);
+    $string = str_replace('   ', ' (', $string . ')');
+    return $string;
+}
+
+function getPlayerName($bondsNr, $seizoen, $title) {
+    $title = str_replace('Resultaten ' . $bondsNr, '', $title);
+    $title = str_replace(' - seizoen ' . $seizoen, '', $title);
+    return trim($title);
+}
+
+function decodeMatches($matches) {
+    $result = [];
+    foreach($matches as $match) {
+        $columns = array_map('getPlainText', $match->find('td'));
+        if(sizeof($columns) == 12) { // Row containing 12 columns are match details
+            list(, $date, , $opponent, $tCar, $car, $turns) = $columns;
+            $match = [
+                'datum' => $date,
+                'opponent' => formatOpponent($opponent),
+                'required' => $tCar,
+                'amount' => $car,
+                'turns' => $turns,
+            ];
+            $result[] = $match;
+        }
+        if(sizeof($columns) == 1 && strpos($columns[0], 'wisseling van spelsoort') !== false) {
+            $result = [];
+        }
+    }
+    return $result;
+}
+
+list($player, $matches) = fetchPlayerAndMatches($page, $bondsNr, $klasse, $seasons, $district);
+$decodedMatches = decodeMatches($matches);
+
+$result = [
+    'player' => $player,
+    'matches' => $decodedMatches,
+];
+echo json_encode($result);
