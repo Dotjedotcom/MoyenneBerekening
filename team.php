4312326efd642d20f4c0e6fa58dbea8133aaac4e
René van Aerle <info@rvgate.nl>
2017-02-02 00:30:33 +0100

    Dynamically find correct column id's from team page

--- team.php
+++ team.php
@@ -19,17 +19,21 @@ $html = file_get_html($url . '?' . http_build_query($query));
 $tables = $html->find('table');
 $teamTable = $tables[1];
 
+$headers = array_map('getPlainText', $teamTable->find('th'));
+$keys = [
+    'bondsNr' => array_search('bondsnr', $headers),
+    'name' => array_search('speler', $headers),
+    'car' => array_search('car', $headers),
+];
 $players = [];
 foreach($teamTable->find('tr') as $rows){
     $columns = array_map('getPlainText', $rows->find('td'));
-    if(sizeof($columns) == 17) {
-        list($bondsNr, $name, , , , , , , , $car) = $columns;
-        $players[intval($bondsNr)] = [
-            'id' => intval($bondsNr),
-            'name' => $name,
-            'required' => intval($car),
-        ];
-    }
+    if (sizeof($columns) < max($keys)) continue;
+    $players[intval($columns[$keys['bondsNr']])] = [
+        'id' => intval($columns[$keys['bondsNr']]),
+        'name' => $columns[$keys['name']],
+        'required' => intval($columns[$keys['car']]),
+    ];
 }
 
 echo json_encode($players);
